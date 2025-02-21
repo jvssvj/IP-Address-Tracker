dc6c0b8e752c1bd47731f76d06202eef2b6d489f
João Vitor <joao7791vitor@gmail.com>
2025-02-20 22:26:20 -0300

    fix-path-images

--- src/scripts/map.ts
+++ src/scripts/map.ts
@@ -28,7 +28,7 @@ export function map(lat: number, lng: number, region: string, country: string) {
     }).addTo(mapInstance);
 
     const customIcon = L.icon({
-        iconUrl: './images/icon-location.svg', // Verifique se o caminho está correto
+        iconUrl: '../../src/images/icon-location.svg',
         iconSize: [42, 42],
         iconAnchor: [16, 52],
         popupAnchor: [5, -40]
